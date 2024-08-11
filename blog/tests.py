4c9e7fd8c517a6020558355dda4b590d694e95d6
Alfredomg7 <alfredomg4000@gmail.com>
2024-08-10 20:02:36 -0700

    Implement add comment funcionality in 'article_detail.html' template and displaying of all the article comments, enhance 'article_form.html' template with responsive containers for form inputs, register comments model in 'admin.py' module, and add respective comments funcionality tests in 'tests.py' module.

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
-from .models import Article
+from .models import Article, Comment
 
 class ArticleTests(TestCase):
     def setUp(self):
@@ -45,3 +45,35 @@ class ArticleTests(TestCase):
         response = self.client.post(reverse('article_delete', args=[self.article.pk]))
         self.assertEqual(response.status_code, 302)
         self.assertFalse(Article.objects.filter(pk=self.article.pk).exists())
+
+class CommentTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.article = Article.objects.create(
+            title='Test Article',
+            content='This is a test article.',
+            author=self.user
+        )
+        self.comment = Comment.objects.create(
+            article=self.article,
+            content='Test comment.',
+            author=self.user
+        )
+        self.client.login(username='testuser', password='12345')
+
+    def test_add_comment(self):
+        response = self.client.post(reverse('article_detail', args=[self.article.pk]), {
+            'content': 'Another test comment.',
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Comment.objects.filter(content='Another test comment.').exists())
+    
+    def test_comment_display(self):
+        Comment.objects.create(
+            article = self.article,
+            author=self.user,
+            content='Test comment.'
+        )
+        response = self.client.get(reverse('article_detail', args=[self.article.pk]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test comment.')
